7ef8a3024ac57e42bc62517ce00acae35c4b9862
MuchiriJohn <muchiri.mwihaki@gmail.com>
2015-06-25 15:19:15 +0300

    temp

--- python/editor/py/temp.py
+++ python/editor/py/temp.py
@@ -4,27 +4,30 @@ from board import *
 board = scratch_n_sketch()
 board.connect()
 board.backGroundColor(0, 0, 0)
-board.textBackColor(0, 0, 0)
-board.penColor(0, 255, 255)
-board.setFont(board.font_small)
+board.textBackColor(50, 50, 50)
+board.penColor(50, 50, 50)
+board.setFont(board.comic_sans) 
 board.rotateDisplay(board.rotate_0)
-board.drawText('Luminance', 20, 30);
-board.drawText('Touch', 95, 100);
-board.drawText('Remote', 85, 170);
-board.drawText('Encoder', 150, 30);
-board.drawText('Button 1', 20, 230);
-board.drawText('Button 2', 150, 230);
-#loop checking sensor data
+board.fillRoundRectangle(10, 250, 115, 300);
+board.fillRoundRectangle(120, 250, 230, 300);
+board.penColor(255, 0, 0)
+board.drawText('On',  50 , 270)
+board.penColor(0, 255, 0)
+board.drawText('Off', 150 , 270)
+board.penColor(255, 255, 0)
+board.textBackColor(0, 0, 0)
+xpos = 0
+ypos = 0
+board.setFont(board.font_small)
 while True:
-    #get sensor data
     board.getSensorData()
-    board.drawText(board.lightSensor, 35, 50)
-    board.drawText('x : {0}  y : {1}'.format(board.touchX, board.touchY), 55, 120)
-    board.drawText(board.remoteCode, 95, 190)
-    board.drawText(board.angleSensor, 155, 50)
-    board.drawToggleIcon(45, 250, board.SwitchOne)
-    board.drawToggleIcon(175, 250, board.SwitchTwo)
-    #wait for 100ms
-    wait(100)
+    xpos = board.touchX
+    ypos = board.touchY
+    board.drawText('x : {0}  y : {1}'.format(xpos, ypos), 50, 120)
+    if (xpos>153 and xpos<482) and (ypos>766 and ypos<910):
+        board.ledWrite(Green, On)
+    if (xpos>527 and xpos<865) and (ypos>766 and ypos<910):
+        board.ledWrite(Green, Off)
+    wait(50)
 #disconnect board
 board.disconnect()
